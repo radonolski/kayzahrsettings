eed6f06f3b34269820681ee34e8989580bef5881
radonolski <carlo.schoenenberger@outlook.com>
2026-08-04 02:21:59 +0200

    Update games.js

--- games.js
+++ games.js
@@ -2,16 +2,9 @@ const games = [
     {
         id: "blackops2",
         title: "Call of Duty: Black Ops II",
-        steamAppId: "202970", 
+        steamAppId: "202970",
         content: `
-            <h3>Maus & Tastatur</h3>
-            <ul>
-                <li>DPI: 800</li>
-                <li>In-Game Sens: 1.5</li>
-                <li>FOV: 90</li>
-            </ul>
-            <h3>Video Settings</h3>
-            <p>Alles auf Low für maximale FPS. Schatten deaktiviert.</p>
+            <h1>Call of Duty: Black Ops II (PS5) Einstellungen:</h1><p><br></p><p><br></p><p><strong>Leistung des Spiels und Input Delay verbessern:</strong></p><p><br></p><p><em>In PS Einstellungen:</em></p><p><br></p><ol><li>Auflösung auf 1080p</li><li>VRR aus</li><li>120Hz aus</li><li>PSSR aus</li><li>ALLM aus</li><li>HDR aus</li><li>PS4 Version nutzen</li><li>Controller per Kabel verwenden</li></ol><p><br></p><p><br></p><p><strong>NAT Typ und Verbindung verbessern, bzw. auf "Offen" stellen:</strong></p><p><br></p><p><em>In den PlayStation Einstellungen (Internetverbindung konfigurieren/erweiterte Einstelllungen):</em></p><p><br></p><ol><li>IP-Adresseinstellungen: Manuell</li><li>IPv4-Adresse: IP Adresse der PS im Router einsehen und hier eintragen; die letzten beiden Zahlen auf "99" ändern</li><li>Subnetzmaske: 255.255.255.0</li><li>Standard-Gateway: IP Adresse des Routers eintragen; (FritzBox: 192.168.178.1; Telekom Router: 192.168.2.1; Vodafone Router: 192.168.0.1)</li><li>Primäre DNS: 8.8.8.8 oder 1.1.1.1</li><li>Sekundäre DNS: 8.8.4.4 oder 1.0.0.1</li></ol><p><br></p><p><em>In den Router Einstellungen:</em></p><p><br></p><ol><li>IPv4-Adresse der PlayStation auf <a href="http://xxx.xxx.xxx" target="_blank">xxx.xxx.xxx</a>.99 ändern</li><li>IPv4-Adresse dauerhaft festlegen</li><li>PlayStation unter Internetnutzung priorisieren</li><li>Ports freigeben: Jeweils TCP und UDP Protokoll: 3074, 3478-3480, 27014-27050</li></ol><p><br></p><p><br></p><p><strong>Weitere Einstellungen:</strong></p><p><br></p><p>Sens: 7</p><p><br></p><p><br></p><p><strong>Wenn sich hierdurch etwas bei euch verbessert haben sollte, lasst gerne etwas Liebe in KayzahRs Chat da :]</strong></p><p><br></p><h1><br></h1><p><br></p>
         `
     },
     {
@@ -19,7 +12,7 @@ const games = [
         title: "Call of Duty: Warzone",
         steamAppId: "1962663",
         content: `
-            <h3>Test</h3><p>Test</p>
+            <h1>Call of Duty: Warzone Einstellungen:</h1><p><br></p><p><strong>Chris hat zu Call of Duty Warzone ein YouTube Video hochgeladen, in dem alle</strong></p><p><strong>relevanten Einstellungen gezeigt werden:</strong></p><p><br></p><p>https://youtu.be/u03TiLVvBmo?si=whUHO1LBxzOpmo1z&nbsp;</p><p><br></p><p><strong>Wenn sich hierdurch etwas bei euch verbessert haben sollte, lasst gerne etwas Liebe in KayzahRs Chat da :]</strong></p><p><br></p><h1><br></h1><p><br></p>
         `
     },
     {
@@ -27,7 +20,7 @@ const games = [
         title: "Battlefield 6",
         steamAppId: "2807960",
         content: `
-            <h1>Battlefield 6 REDSEC Einstellungen:</h1><p><br></p><p><br></p><p><strong>Chris hat zu seinen REDSEC Einstellungen ein YouTube Video hochgeladen:</strong></p><p><br></p><p>https://www.youtube.com/watch?v=1_yDKgUD0LA</p><p><br></p><h1><br></h1><p><br></p>
+            <h1>Batllefield 6/REDSEC Einstellungen:</h1><p><br></p><p><strong>Chris hat zu Battlefield 6/REDSEC ein YouTube Video hochgeladen, in dem alle</strong></p><p><strong>relevanten Einstellungen gezeigt werden:</strong></p><p><br></p><p>https://www.youtube.com/watch?v=1_yDKgUD0LA&nbsp;</p><p><br></p><p><strong>Wenn sich hierdurch etwas bei euch verbessert haben sollte, lasst gerne etwas Liebe in KayzahRs Chat da :]</strong></p><p><br></p><h1><br></h1><p><br></p>
         `
     },
 ];
